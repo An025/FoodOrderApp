be190e0eb4d9787713d5e14d6e5b923586724f32
Ani <jancso.anita@gmail.com>
2021-10-31 15:21:44 +0100

    Add HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,13 @@
 import React, {Fragment} from 'react';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = props =>{
     return <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
+            <HeaderCartButton/>
+
         </header>
 
     </Fragment>
